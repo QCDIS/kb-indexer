960cc404badf1b6ab66e10f5b45fe52f769b0c61
Gabriel Pelouze <gabriel@pelouze.net>
2023-01-23 09:57:08 +0100

    rewrite notebook records ingestion

--- indexers/notebook/indexing.py
+++ indexers/notebook/indexing.py
@@ -1,142 +1,45 @@
-from elasticsearch_dsl import Index
+import json
 import os
 
-import pandas as pd
 from tqdm import tqdm
-
-from elasticsearch import Elasticsearch
 
 from .. import utils
 
 
-# ----------------------------------------------------------------------------------
+class JsonIndexer(utils.ElasticsearchIndexer):
 
-class ElasticsearchIndexer():
-    '''Index preprocessed notebooks with Elasticsearch.
+    def __init__(self, index_name, json_dir):
+        super().__init__(index_name)
+        self.json_dir = json_dir
 
-    Attrs:
-        - es: Elasticsearch client
-        - index_name: Elasticsearch index name.
-        - notebook_path: The path for storing notebook files.
-        - source_name: 'Kaggle' or 'Github'. The repository source for notebooks
-        - doc_type: 'preprocessed' or 'raw'. The document type for notebooks
-    '''
-
-    def __init__(
-            self, es: Elasticsearch, source_name: str, doc_type: str,
-            index_name: str, id_key: str,
-            ):
-        self.es = es
-        self.source_name = source_name
-        self.doc_type = doc_type
-        self.index_name = index_name
-        self.id_key = id_key
-
-        self.notebook_path = os.path.join(
-            os.path.dirname(__file__),
-            f'data/{self.source_name}/notebook_lists',
-            )
-        if doc_type == 'raw':
-            source_file_name = 'raw_notebooks.csv'
-        elif doc_type == 'preprocessed':
-            source_file_name = 'updated_processed_notebooks.csv'
-        else:
-            raise ValueError(f'unknown doc_type: {doc_type}')
-        self.source_file_name = os.path.join(
-            self.notebook_path,
-            source_file_name
-            )
-
-    def generate_index_files(self) -> list:
-        ''' Generate a list of index files to be indexed by Elasticsearch.
-
-        Each index file is in the form of dictionary.
-        Depending on the source name, the index uses different schema.
-        '''
-        indexfiles = []
-        # Index preprocessed notebooks
-        if self.doc_type == 'preprocessed':
-            # ['source_id', 'name', 'file_name', 'html_url', 'description', 'source', 'docid', 'language',
-            # 'num_cells', 'num_code_cells', 'num_md_cells', 'len_md_text']
-            df_notebooks = pd.read_csv(
-                os.path.join(self.notebook_path, self.source_file_name)
-                )
-            indexfiles = df_notebooks.to_dict('records')
-
-        # Index raw notebooks
-        # ['docid', 'source_id', 'name', 'file_name', 'source', 'notebook_source_file']
-        elif self.doc_type == 'raw':
-            root = self.notebook_path
-            df_notebooks = pd.read_csv(
-                os.path.join(self.notebook_path, self.source_file_name)
-                )
-            indexfiles = df_notebooks.to_dict('records')
-        else:
-            print("Notebook type is unknown, please specify a doc_type.")
-        # replace nan values with None
-        for record in indexfiles:
-            for k, v in record.items():
-                if pd.isna(v):
-                    record[k] = None
-        return indexfiles
-
-    def index_notebooks(self, reindex=False):
-        ''' Index generated files into Elasticsearch database given index name
-
-        Can index raw notebooks and preprocessed notebooks.
-
-        When indexing raw notebooks, it requires a `kaggle_raw_notebooks.csv` file placed under `notebook_path`
-        '''
-        indexer = utils.ElasticsearchIndexer(self.index_name)
-
-        # Call Elasticsearch to index the files
-        indexfiles = self.generate_index_files()
-        for record in tqdm(indexfiles, desc='Indexing notebooks'):
-            indexer.ingest_record(record[self.id_key], record)
-        return True
-
-
-# ----------------------------------------------------------------------------------
+    def bulk_ingest(self):
+        for record_file in tqdm(os.listdir(self.json_dir),
+                                desc='ingesting records'):
+            record_file = os.path.join(self.json_dir, record_file)
+            if not os.path.splitext(record_file)[1] == '.json':
+                print('skipping', record_file)
+                continue
+            with open(record_file, 'r') as f:
+                record = json.load(f)
+            self.ingest_record(record['id'], record)
 
 
 def main():
-    es = utils.create_es_client()
+    sources = [
+        'Kaggle',
+        'Github',
+        ]
 
-    indexer = ElasticsearchIndexer(
-        es=es,
-        source_name="Kaggle",
-        doc_type="preprocessed",
-        index_name="kaggle_notebooks",
-        id_key="docid",
-        )
-    indexer.index_notebooks(reindex=False)
-
-    indexer = ElasticsearchIndexer(
-        es=es,
-        source_name="Kaggle",
-        doc_type="raw",
-        index_name="kaggle_raw_notebooks",
-        id_key="docid",
-        )
-    indexer.index_notebooks(reindex=False)
-
-    # indexer = ElasticsearchIndexer(
-    #     es=es,
-    #     source_name="Github",
-    #     doc_type="preprocessed",
-    #     index_name="github_notebooks",
-    #     id_key="git_url",
-    #     )
-    # indexer.index_notebooks(reindex=False)
-    #
-    # indexer = ElasticsearchIndexer(
-    #     es=es,
-    #     source_name="Github",
-    #     doc_type="raw",
-    #     index_name="github_raw_notebooks",
-    #     id_key="git_url",
-    #     )
-    # indexer.index_notebooks(reindex=False)
+    for source in sources:
+        print(f'Ingesting notebooks from {source}')
+        data_dir = os.path.join(
+            os.path.dirname(__file__),
+            f'data/{source}/repositories_metadata/')
+        indexer = JsonIndexer(
+            'notebooks',
+            data_dir,
+            )
+        indexer.bulk_ingest()
 
 
 if __name__ == '__main__':
