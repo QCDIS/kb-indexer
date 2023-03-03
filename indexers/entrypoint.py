acea7f81047e93e99f4dc5030ca933c3f54cf9b9
Gabriel Pelouze <gabriel@pelouze.net>
2023-03-03 17:05:37 +0100

    add dapi entrypoint

--- indexers/entrypoint.py
+++ indexers/entrypoint.py
@@ -2,12 +2,17 @@
 
 import click
 
+from .api.entrypoint import api
+
 
 @click.group(
     help='Indexing pipeline for the ENVRI Knowledge Base',
     )
 def cli():
     pass
+
+
+cli.add_command(api)
 
 
 if __name__ == '__main__':
